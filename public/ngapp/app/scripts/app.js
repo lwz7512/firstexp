007e962e841c7c2a99685b8ae03ed47b9d6ba89c
lwz7512 <lwz7512@gmail.com>
2014-01-04 16:01:29 +0800

    add animation...

--- public/ngapp/app/scripts/app.js
+++ public/ngapp/app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('ngappApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ngAnimate'
 ])
   .config(function ($routeProvider) {
     $routeProvider
